f3062d759cdd431ef67b87f69d0943c22d49dbf3
Maxwell Young <maxtheyoung@gmail.com>
2025-04-25 13:57:50 +1200

    Remove Wintour component from layout to simplify structure and enhance focus on main content. This change contributes to a cleaner user interface while retaining essential functionality.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -214,7 +214,6 @@ export default function RootLayout({
         />
       </head>
       <body className="overflow-x-hidden">
-        <Wintour />
         {children}
         <Analytics />
       </body>
