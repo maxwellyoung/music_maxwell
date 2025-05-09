4c4eae2bc2e119f952eb5e648532564582e38995
Maxwell Young <maxtheyoung@gmail.com>
2025-05-10 10:35:07 +1200

    Update build script and enforce dynamic behavior in authentication routes
    - Modified the build script in package.json to skip environment validation during the build process.
    - Added dynamic and runtime exports to the NextAuth and set-username API routes to enforce dynamic behavior and specify Node.js runtime.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,9 @@ import NextAuth from "next-auth";
 import { authOptions } from "~/lib/auth";
 import type { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 const handler = NextAuth(authOptions);
 
 export const GET: (req: NextRequest) => Promise<Response> = handler;
