b6bef79dadb9f3f2a36e65a1db32cc1a9af1b3ab
Maxwell Young <maxtheyoung@gmail.com>
2025-05-14 13:18:28 +1200

    Refactor user authentication flow by updating NEXTAUTH_URL in .env.development, enhancing error handling in the set-username POST handler, and improving session management in AuthForm. Additionally, add a settings button in the Navbar for authenticated users.

---
+++ src/components/icons/YoutubeIcon.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import MyIcon from "../../../public/icons/yt.svg";
+
+const YoutubeIcon = () => (
+  <div>
+    <MyIcon className="h-6 w-6 text-accent" />
+  </div>
+);
+
+export default YoutubeIcon;
